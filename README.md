3fcad2826d65ed4eb13021becf1a16927557d97b
DanyCarter <danielcadenas88@gmail.com>
2023-12-04 09:00:24 +0100

    hemos creado el vehiculo y el README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Proyecto para el examen del Tema4 (DWEC/DIW)
+
+**Nombre**: Daniel Cadenas Dominguez
+**GitHub**:
+
+##Examen
+1.DWEC
+2.DIW
--- scripts/vehiculo.js
+++ scripts/vehiculo.js
@@ -4,5 +4,55 @@ class vehiculo {
     #color;
     #añoFabricacion;
     #cilindrada;
+
+
+constructor (marca, modelo, color, añoFabricacion, cilindrada) {
+    this.#marca = marca;
+    this.#modelo = modelo;
+    this.#color = color;
+    this.#añoFabricacion = añoFabricacion;
+    this.#cilindrada = cilindrada;
+ }
+get marca(){
+    return this.#marca;
 }
 
+set marca(marca) {
+    this.#marca = marca;
+}
+
+get modelo(){
+    return this.#modelo
+}
+
+set modelo(modelo){
+    this.#modelo = modelo
+}
+
+get color(){
+    return this.#modelo;
+}
+
+set color(color){
+    this.#color = color;
+}
+
+get añoFabricacion(){
+    return this.#añoFabricacion;
+}
+
+set añoFabricacion(añoFabricacion){
+    this.#añoFabricacion = añoFabricacion;
+}
+
+get cilindrada(){
+    return this.#cilindrada;
+}
+
+set cilindrada(cilindrada){
+    this.#añoFabricacion = cilindrada;
+}
+
+
+
+}
